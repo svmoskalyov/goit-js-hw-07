afc6a2bc53d8d900d83154890ad33396460dc4e3
Sergei Moskalyov <sv.moskalyov@gmail.com>
2022-09-21 16:04:20 +0300

    Створює і рендерить галерею

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-
 console.log(galleryItems);
+
+const galleryBox = document.querySelector('.gallery');
+console.log('🚀 ~ galleryBox', galleryBox);
+
+galleryBox.addEventListener('click', onImgClick);
+
+function makeGalleryMarkup(element) {
+  const markup = element
+    .map(({ preview, original, description }) => {
+      return /*html*/ `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}">
+    </a>
+    </div>`;
+    })
+    .join('');
+
+  galleryBox.insertAdjacentHTML('beforeend', markup);
+}
+
+makeGalleryMarkup(galleryItems);
