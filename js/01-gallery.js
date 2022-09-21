c0a5030d1a896f345fbfc1afbb26d617fa1233c1
Sergei Moskalyov <sv.moskalyov@gmail.com>
2022-09-21 16:05:34 +0300

    Отримує url великого зображення

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -21,3 +21,86 @@ function makeGalleryMarkup(element) {
 }
 
 makeGalleryMarkup(galleryItems);
+
+
+function onImgClick(event) {
+    event.preventDefault();
+    // console.log(event.target.closest('.gallery__link'));
+    // console.log(event.target);
+    const imgLargeLink = event.target.dataset.source;
+    console.log("🚀 ~ onImgClick ~ imgLargeLink", imgLargeLink)
+    // console.log(event.target.dataset.source);
+    // console.log(event.target.nodeName);
+    // console.log(event.target.);
+   
+
+
+
+}
+
+
+// const instance = basicLightbox.create(
+//   `
+//     <div class="modal">
+//         <p>A custom modal that has been styled independently. It's not part of basicLightbox, but perfectly shows its flexibility.</p>
+//         <input placeholder="Type something">
+//         <a>Close</a>
+//     </div>
+// `,
+//   {
+//     onShow: instance => {
+//       instance.element().querySelector('a').onclick = instance.close;
+//     },
+//   }
+// );
+
+// instance.show();
+
+
+// <div class="gallery__item">
+//   <a class="gallery__link" href="large-image.jpg">
+//     <img
+//       class="gallery__image"
+//       src="small-image.jpg"
+//       data-source="large-image.jpg"
+//       alt="Image description"
+//     />
+//   </a>
+// </div>
+
+// const instance = basicLightbox.create(`
+//     <div class="modal">
+//   <img src="assets/images/image.png" width="420" height="320">
+//     </div>
+
+// `);
+
+// instance.show();
+
+// const instance = basicLightbox.create(`
+//     <div class="modal">
+//         <p>
+//             Your first lightbox with just a few lines of code.
+//             Yes, it's really that simple.
+//         </p>
+//     </div>
+// `);
+
+// instance.show();
+
+// const instance = basicLightbox.create(
+//   `
+//     <div class="modal">
+//         <p>A custom modal that has been styled independently. It's not part of basicLightbox, but perfectly shows its flexibility.</p>
+//         <input placeholder="Type something">
+//         <a>Close</a>
+//     </div>
+// `,
+//   {
+//     onShow: instance => {
+//       instance.element().querySelector('a').onclick = instance.close;
+//     },
+//   }
+// );
+
+// instance.show();
