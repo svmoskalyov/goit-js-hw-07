3477e6f0b7f21e62039baf17e4f1f066e9d43e7d
Sergei Moskalyov <sv.moskalyov@gmail.com>
2022-09-21 18:02:13 +0300

    Підключає стилі basicLightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,15 +2,14 @@ import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
 
 const galleryBox = document.querySelector('.gallery');
-console.log('🚀 ~ galleryBox', galleryBox);
 
-galleryBox.addEventListener('click', onImgClick);
+makeGalleryMarkup(galleryItems);
 
 function makeGalleryMarkup(element) {
   const markup = element
     .map(({ preview, original, description }) => {
       return /*html*/ `<div class="gallery__item">
-    <a class="gallery__link" href="${original}">
+    <a class="gallery__link"lbox" href="${original}">
     <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}">
     </a>
     </div>`;
@@ -19,88 +18,3 @@ function makeGalleryMarkup(element) {
 
   galleryBox.insertAdjacentHTML('beforeend', markup);
 }
-
-makeGalleryMarkup(galleryItems);
-
-
-function onImgClick(event) {
-    event.preventDefault();
-    // console.log(event.target.closest('.gallery__link'));
-    // console.log(event.target);
-    const imgLargeLink = event.target.dataset.source;
-    console.log("🚀 ~ onImgClick ~ imgLargeLink", imgLargeLink)
-    // console.log(event.target.dataset.source);
-    // console.log(event.target.nodeName);
-    // console.log(event.target.);
-   
-
-
-
-}
-
-
-// const instance = basicLightbox.create(
-//   `
-//     <div class="modal">
-//         <p>A custom modal that has been styled independently. It's not part of basicLightbox, but perfectly shows its flexibility.</p>
-//         <input placeholder="Type something">
-//         <a>Close</a>
-//     </div>
-// `,
-//   {
-//     onShow: instance => {
-//       instance.element().querySelector('a').onclick = instance.close;
-//     },
-//   }
-// );
-
-// instance.show();
-
-
-// <div class="gallery__item">
-//   <a class="gallery__link" href="large-image.jpg">
-//     <img
-//       class="gallery__image"
-//       src="small-image.jpg"
-//       data-source="large-image.jpg"
-//       alt="Image description"
-//     />
-//   </a>
-// </div>
-
-// const instance = basicLightbox.create(`
-//     <div class="modal">
-//   <img src="assets/images/image.png" width="420" height="320">
-//     </div>
-
-// `);
-
-// instance.show();
-
-// const instance = basicLightbox.create(`
-//     <div class="modal">
-//         <p>
-//             Your first lightbox with just a few lines of code.
-//             Yes, it's really that simple.
-//         </p>
-//     </div>
-// `);
-
-// instance.show();
-
-// const instance = basicLightbox.create(
-//   `
-//     <div class="modal">
-//         <p>A custom modal that has been styled independently. It's not part of basicLightbox, but perfectly shows its flexibility.</p>
-//         <input placeholder="Type something">
-//         <a>Close</a>
-//     </div>
-// `,
-//   {
-//     onShow: instance => {
-//       instance.element().querySelector('a').onclick = instance.close;
-//     },
-//   }
-// );
-
-// instance.show();
